42b7f42366dc992998b86bd75412fb4f5ee94eb4
Whatsm97 <gambacortagiuseppe@outlook.it>
2025-04-14 11:37:47 +0200

    now the footer is always down

--- src/template/base.php
+++ src/template/base.php
@@ -11,6 +11,21 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <title><?php echo $templateParams["titolo"]; ?></title>
+
+
+    <style>
+        /* Sticky footer styles */
+        html, body {
+            height: 100%;
+        }
+        body {
+            display: flex;
+            flex-direction: column;
+        }
+        main {
+            flex: 1;
+        }
+    </style>
 </head>
 
 <body>
