2ef86db77eb3f9ecd05d40625dc8f7dd12d98db0
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2025-04-30 11:37:41 +0200

    refine methods

--- src/index.php
+++ src/index.php
@@ -4,8 +4,7 @@ require_once 'bootstrap.php';
 $templateParams["titolo"] = "ChapterOne - Home";
 $templateParams["nome"] = "homepage.php";
 $templateParams["categorie"] = $dbh->getCategories();
-$templateParams["bestsellers"] = $dbh->getBooks();
-//$templateParams["novità"] = $dbh->getNewBooks(10);
+$templateParams["novità"] = $dbh->getNewBooks(10);
 $templateParams["bestsellers"] = $dbh->getbestSellers(10);
 
 require 'template/base.php';
