6fa8aa8318f4844acfdefe74d731d4099de3bda1
Whatsm97 <gambacortagiuseppe@outlook.it>
2025-04-30 10:34:14 +0200

    arm emulation

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@ $templateParams["titolo"] = "ChapterOne - Home";
 $templateParams["nome"] = "homepage.php";
 $templateParams["categorie"] = $dbh->getCategories();
 $templateParams["bestsellers"] = $dbh->getBooks();
-$templateParams["novità"] = $dbh->getNewBooks(10);
+//$templateParams["novità"] = $dbh->getNewBooks(10);
 $templateParams["bestsellers"] = $dbh->getbestSellers(10);
 
 require 'template/base.php';
