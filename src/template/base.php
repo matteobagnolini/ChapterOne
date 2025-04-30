01bbe70edd4f6def8706728d07c8c1d103732e45
Whatsm97 <gambacortagiuseppe@outlook.it>
2025-04-30 14:51:19 +0200

    removed icons from navbar if admin logged

--- src/template/base.php
+++ src/template/base.php
@@ -34,8 +34,11 @@
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php">ChapterOne Shop</a>
                     <div class="d-flex align-items-center order-lg-2">
-                    <a class="nav-link me-2" href="notifiche.php"><i class="bi bi-bell me-1"></i> Notifiche</a>
-                    <a class="nav-link me-2" href="cart.php"><i class="bi bi-cart me-1"></i> Carrello</a>
+                    
+                    <?php if(!isset($_SESSION['admin']) || (isset($_SESSION['admin']) && !$_SESSION['admin'])): ?>
+                        <a class="nav-link me-2" href="notifiche.php"><i class="bi bi-bell me-1"></i> Notifiche</a>
+                        <a class="nav-link me-2" href="cart.php"><i class="bi bi-cart me-1"></i> Carrello</a>
+                    <?php endif; ?>
                     <?php if(!isset($_SESSION['username'])): ?>
                         <a class="nav-link me-2" href="login.php"><i class="bi bi-box-arrow-in-right me-1"></i> Login</a>
                     <?php elseif(isset($_SESSION['admin']) && $_SESSION['admin']): ?>
