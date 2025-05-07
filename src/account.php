da971aa091802cfa35d66e5ac23d2a3fc92e18ec
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2025-05-07 21:58:29 +0200

    fix security issues with account and adminaccount

--- src/account.php
+++ src/account.php
@@ -5,9 +5,11 @@ $templateParams["titolo"] = "ChapterOne - Home";
 $templateParams["nome"] = "accountpage.php";
 $templateParams["categorie"] = $dbh->getCategories();
 
-$templateParams["accountInfo"] = $dbh->getAccountInfo($_SESSION["username"]); # TODO: Account info with logged user ID
-# Add upload of new values from form
-
+if(isUserLoggedIn()){
+    $templateParams["accountInfo"] = $dbh->getAccountInfo($_SESSION["username"]); # TODO: Account info with logged user ID
+}else{
+    header("location: login.php");
+} 
 
 require 'template/base.php';
 
