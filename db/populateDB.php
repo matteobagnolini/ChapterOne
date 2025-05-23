840db0bc1e803e72b99ad4e24dc240d0bef915aa
Whatsm97 <gambacortagiuseppe@outlook.it>
2025-05-23 19:52:54 +0200

    fix db init prices

--- db/populateDB.php
+++ db/populateDB.php
@@ -97,8 +97,8 @@ try {
     $discountCodeId3 = $db->insertDiscountCode('FIXED15', 'fixed', 15.00, '2024-01-01', '2025-12-31', false, true);
     
     // Crea alcuni ordini
-    $orderId1 = $db->insertOrder('2025-03-15 10:30:00', 19.99, $customerId1, null);
-    $orderId2 = $db->insertOrder('2025-03-20 14:45:00', 12.00, $customerId2, $discountCodeId1);
+    $orderId1 = $db->insertOrder('2025-03-15 10:30:00', 63.99, $customerId1, null);
+    $orderId2 = $db->insertOrder('2025-03-20 14:45:00', 40.98, $customerId2, $discountCodeId1);
     $orderId3 = $db->insertOrder('2025-03-25 09:15:00', 55.50, $customerId3, null);
     
     
